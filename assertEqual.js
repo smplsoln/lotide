2a1814b91ed81ef3f1ad82351ef98a581d38ec12
Prashant R <smplsoln@gmail.com>
2021-09-14 12:01:28 +0000

    Add assert equals JS with emojis

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+
+  let failEmoji = '🤬 🤬 🤬';
+  let passEmoji = '💚 💚 💚';
+  if (actual === expected) {
+    // console.assert(true, "The actual value is equal to the expected value.");
+    console.log('Assertion Passed: %s [%s] !== [%s]', passEmoji, actual, expected);
+  } else {
+    // console.assert(false, "The actual value is different from the expected value.");
+    console.log('Assertion Failed: %s [%s] !== [%s]', failEmoji, actual, expected);
+  }
+};
+
+// TEST CODE
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Bootcamp");
+
