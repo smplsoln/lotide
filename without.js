92e1e9a822e0acab4c38a1039f70fcb07d1f116c
Prashant R <smplsoln@gmail.com>
2021-09-14 22:18:02 +0000

    Add test for verifying source array remains intact after without call in without js

--- without.js
+++ without.js
@@ -88,3 +88,8 @@ assertEqual(without(NaN, ["1", "2", "3"]), undefined);
 assertEqual(without(123, [1, 2, 3]), undefined);
 assertEqual(without(true, ["1", "2", "3"]), undefined);
 assertEqual(without(new String(123), ["1", "2", "3"]), undefined);
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
