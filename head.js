e9624a51d799221ee5f57c79c6c19b7bf46470c4
Prashant R <smplsoln@gmail.com>
2021-09-14 12:24:38 +0000

    Add implementation with template literals

---
+++ head.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+
+  let failEmoji = '🤬 🤬 🤬';
+  let passEmoji = '💚 💚 💚';
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${passEmoji} [${actual}] !== [${expected}]`);
+  } else {
+    console.log(`Assertion Failed: ${failEmoji} [${actual}] !== [${expected}]`);
+  }
+};
+
+function head(arr) {
+  return (null === arr) ? null
+    : (undefined === arr) ? undefined
+      : arr[0];
+}
+
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+assertEqual(head(["Hello"]), "Hello");
+assertEqual(head([]), undefined);
+assertEqual(head(null), null);
+
