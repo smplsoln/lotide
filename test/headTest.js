b3c203d42dd265775359decb144349613e472626
Prashant R <smplsoln@gmail.com>
2021-09-20 17:20:18 -0600

    add npm support and \n add mocha and chai as test dependencies and \n also convert headtTest to use mocha and chai apis

--- test/headTest.js
+++ test/headTest.js
@@ -1,10 +1,32 @@
 const assertEqual = require('../assertEqual');
 const head = require('../head');
 
-// TEST CODE for head.js
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+const assert = require('chai').assert;
 
-assertEqual(head(["Hello"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head(null), null);
+// TEST CODE for head.js
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns \"Hello\" for [\"Hello\", \"Lighthouse\", \"Labs\"]", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it("returns \"Hello\" for [\"Hello\"]", () => {
+    assert.strictEqual(head(["Hello"]), "Hello");
+  });
+
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+  it("returns null for null", () => {
+    assert.strictEqual(head(null), null);
+  });
+});
