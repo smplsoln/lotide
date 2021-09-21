c95dba9b155d7c91660f7163f82111068504cb37
Prashant R <smplsoln@gmail.com>
2021-09-20 18:11:42 -0600

    refactor tail and middle js tests with mocha and  chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,25 +1,55 @@
-const assertArraysEqual = require('../assertArraysEqual');
-
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
 const middle = require('../middle');
 
-assertArraysEqual(middle([6, 7, 8, 9, 1, 24, 3, 4, 5, 10, 11]), [24]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]);
-assertArraysEqual(middle([1, 3]), []);
-assertArraysEqual(middle([1, 2, 2, 4]), [2, 2]);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, '2', 3]), ['2']);
-assertArraysEqual(middle(["1", "2", "3"]), ["2"]);
+describe("#middle", () => {
 
-assertEqual(middle("123"), undefined);
-assertEqual(middle(null), undefined);
-assertEqual(middle(NaN), undefined);
-assertEqual(middle(123), undefined);
-assertEqual(middle(true), undefined);
-assertEqual(middle(new String(123)), undefined);
+  it("returns [24] for [6, 7, 8, 9, 1, 24, 3, 4, 5, 10, 11]", () => {
+    assert.deepEqual(middle([6, 7, 8, 9, 1, 24, 3, 4, 5, 10, 11]), [24]);
+  });
 
-const words = ["hello", "world", "lighthouse"];
-middle(words); // no need to capture return value for this test case
-// Make sure the original array was not altered by the middle function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+  it("returns [5, 6] for [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]);
+  });
+  it("returns [] for [1, 3]", () => {
+    assert.deepEqual(middle([1, 3]), []);
+  });
+  it("returns [2, 2] for [1, 2, 2, 4]", () => {
+    assert.deepEqual(middle([1, 2, 2, 4]), [2, 2]);
+  });
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns ['2'] for [1, '2', 3]", () => {
+    assert.deepEqual(middle([1, '2', 3]), ['2']);
+  });
+  it("returns [\"2\"] for [\"1\", \"2\", \"3\"]", () => {
+    assert.deepEqual(middle(["1", "2", "3"]), ["2"]);
+  });
+
+  it("returns undefined for \"123\"", () => {
+    assert.strictEqual(middle("123"), undefined);
+  });
+  it("returns undefined for null", () => {
+    assert.strictEqual(middle(null), undefined);
+  });
+  it("returns undefined for null", () => {
+    assert.strictEqual(middle(NaN), undefined);
+  });
+  it("returns undefined for 123", () => {
+    assert.strictEqual(middle(123), undefined);
+  });
+  it("returns undefined for true", () => {
+    assert.strictEqual(middle(true), undefined);
+  });
+  it("returns undefined for new String(123)", () => {
+    assert.strictEqual(middle(new String(123)), undefined);
+  });
+
+  it("does not change the original array argument", () => {
+    const words = ["hello", "world", "lighthouse"];
+    middle(words); // no need to capture return value for this test case
+    // Make sure the original array was not altered by the middle function
+    assert.deepEqual(words, ["hello", "world", "lighthouse"]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,33 @@
-const assertEqual = require('../assertEqual');
-const assertArraysEqual = require('../assertArraysEqual');
 const tail = require('../tail');
 
-// TEST CODE for tail.js
-assertArraysEqual(tail([5,6,7]), [6, 7]);
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertArraysEqual(result, ["Lighthouse", "Labs"]);
+const assert = require('chai').assert;
 
-assertArraysEqual(tail(["Hello"]), []);
-assertArraysEqual(tail([]), []);
-assertEqual(tail(undefined), undefined);
-assertEqual(tail(null), null);
+// TEST CODE for tail.js
+
+describe("#tail", () => {
+
+  it("returns [6, 7] for [5, 6, 7]", () => {
+    assert.deepEqual(tail([5, 6, 7]), [6, 7]);
+  });
+
+  it("returns [\"Lighthouse\", \"Labs\"] for [\"Hello\", \"Lighthouse\", \"Labs\"]", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+
+  it("returns [] for [\"Hello\"]", () => {
+    assert.deepEqual(tail(["Hello"]), []);
+  });
+
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+
+  it("returns undefined for undefined", () => {
+    assert.deepEqual(tail(undefined), undefined);
+  });
+
+  it("returns null for null", () => {
+    assert.deepEqual(tail(null), null);
+  });
+
+});
